c0b7614df167e0dd8702092a83dbcc82ea0e2c75
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-06-02 20:47:53 -0700

    Introduce property file for tests.

--- src/test/java/service/support/ApiTest.java
+++ src/test/java/service/support/ApiTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +16,7 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "classpath:data.sql")
 public abstract class ApiTest {
     protected ApiTest() {
