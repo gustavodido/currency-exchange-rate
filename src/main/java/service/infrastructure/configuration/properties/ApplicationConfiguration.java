9bf9d64bd25e9e362a912600d52bf7479f8adfc3
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-06-03 22:03:15 -0700

    Update property processors.

--- src/main/java/service/infrastructure/configuration/properties/ApplicationConfiguration.java
+++ src/main/java/service/infrastructure/configuration/properties/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package service.infrastructure.configuration.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Data
 public class ApplicationConfiguration {
+    @NestedConfigurationProperty
     private final UrlsConfiguration urls = new UrlsConfiguration();
+    @NestedConfigurationProperty
     private final SchedulingConfiguration scheduling = new SchedulingConfiguration();
 }
